0c0eb263be2a5b437987a2749116866954f815bd
Erin Smith <erin.acumen@gmail.com>
2020-10-08 15:59:14 -0700

    Updates

---
+++ precache-manifest.599105a28098bc63a75f6dbd3755fd12.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a09784ab3f83d60cc4b79080117bd472",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c23be81a86de772d7151",
+    "url": "/portfolio/static/css/main.0d457ed4.chunk.css"
+  },
+  {
+    "revision": "399ad0ecc11e49ade14a",
+    "url": "/portfolio/static/js/2.9e8a0772.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.9e8a0772.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c23be81a86de772d7151",
+    "url": "/portfolio/static/js/main.47d20035.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "9f70211ab8f0a3323e41696f0b65ca07",
+    "url": "/portfolio/static/media/achilles.9f70211a.png"
+  },
+  {
+    "revision": "4433c41bd17b899ba552855a073512ec",
+    "url": "/portfolio/static/media/berry.4433c41b.jpg"
+  },
+  {
+    "revision": "1d013822d776916131316dc4dfba50fd",
+    "url": "/portfolio/static/media/burgerShot.1d013822.png"
+  },
+  {
+    "revision": "f9ffcf816035b069808ab2c929927935",
+    "url": "/portfolio/static/media/cards.f9ffcf81.png"
+  },
+  {
+    "revision": "6e02ad6cfcd32bf8170df6706fcdcc0c",
+    "url": "/portfolio/static/media/erin.6e02ad6c.jpg"
+  },
+  {
+    "revision": "f3835ba0e2a970068c6ca196f513e634",
+    "url": "/portfolio/static/media/game.f3835ba0.png"
+  },
+  {
+    "revision": "dcc0a60536504466a192624d801e015c",
+    "url": "/portfolio/static/media/loo_logo.dcc0a605.jpg"
+  },
+  {
+    "revision": "019e148d027e06b645ae054b7d2a8b3e",
+    "url": "/portfolio/static/media/myfirstpage.019e148d.png"
+  },
+  {
+    "revision": "2bfbdc59a765b7d76c012234ad8afe67",
+    "url": "/portfolio/static/media/profile.2bfbdc59.JPG"
+  },
+  {
+    "revision": "8193f186276f4a7b361573d8d3a14a92",
+    "url": "/portfolio/static/media/purplepages.8193f186.png"
+  },
+  {
+    "revision": "e8b7c0728873af1794c71b1c3f2eb7d2",
+    "url": "/portfolio/static/media/quiz.e8b7c072.png"
+  },
+  {
+    "revision": "030ee21f1d981fed05f0b4460f1179ab",
+    "url": "/portfolio/static/media/scheduler.030ee21f.png"
+  },
+  {
+    "revision": "355c26d408a3121066f30b23d4145f3c",
+    "url": "/portfolio/static/media/team.355c26d4.png"
+  }
+]);
